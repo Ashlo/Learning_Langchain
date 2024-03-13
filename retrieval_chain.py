c84951325d75d38571c76e3b314ef23e02407bea
Ashlo <ashutoshbele5@gmail.com>
2024-03-13 19:02:41 +0530

    doc retrieval chain

--- retrieval_chain.py
+++ retrieval_chain.py
@@ -1,11 +1,15 @@
+from langchain_core.documents import Document
 from langchain_community.document_loaders import WebBaseLoader
 from langchain_community.vectorstores import FAISS
 from langchain_text_splitters import RecursiveCharacterTextSplitter
-
-
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_community.llms import Ollama
+from langchain.chains import create_retrieval_chain
 
 loader = WebBaseLoader("https://docs.smith.langchain.com/user_guide")
 
+llm = Ollama(model='llama2')
 
 docs = loader.load()
 
@@ -16,6 +20,23 @@ text_splitter = RecursiveCharacterTextSplitter()
 documents = text_splitter.split_documents(docs)
 vector = FAISS.from_documents(documents, embeddings)
 
-print(vector)
-print("+++++++++++++++++++++++++++++++++++++++++")
-print(documents)
+prompt = ChatPromptTemplate.from_template("""Answer the following question based only on the provided context:
+
+<context>
+{context}
+</context>
+
+Question: {input}""")
+
+document_chain = create_stuff_documents_chain(llm, prompt)
+document_chain.invoke({
+    "input": "how cam langsmith help with building SaaS",
+    "context": [Document(page_content="Langsmith can help you create SaaS from Scratch")]
+})
+
+retriever = vector.as_retriever()
+retrieval_chain = create_retrieval_chain(retriever, document_chain)
+
+response = retrieval_chain.invoke({"input": "how can langsimth help with building SaaS"})
+if response:
+    print(response['answer'])
