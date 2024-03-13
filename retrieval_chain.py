16c6ddef73f1ffce4aa5fa4b099ad0d06875a16f
Ashlo <ashutoshbele5@gmail.com>
2024-03-13 14:53:07 +0530

    vector

--- retrieval_chain.py
+++ retrieval_chain.py
@@ -1,5 +1,9 @@
-
 from langchain_community.document_loaders import WebBaseLoader
+from langchain_community.vectorstores import FAISS
+
+
+
+
 loader = WebBaseLoader("https://docs.smith.langchain.com/user_guide")
 
 
@@ -8,5 +12,9 @@ docs = loader.load()
 from langchain_community.embeddings import OllamaEmbeddings
 
 embeddings = OllamaEmbeddings()
+text_splitter = RecursiveCharacterTextSplitter()
+documents = text_splitter.split_documents(docs)
+vector = FAISS.from_documents(documents, embeddings)
 
-print(embeddings)
+print(vector)
+print(documents)
